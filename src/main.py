52f3ff1cd89297d840414655620dbbbe4eb78010
Pogrammerr <canozfuttu@hotmail.com>
2022-01-14 20:13:28 +0300

    unnecessary imports deleted

--- src/main.py
+++ src/main.py
@@ -1,10 +1,4 @@
-from PyQt5 import QtWidgets
-import pyrebase
-from config.firebase import getFirebaseConfig
 from windows.loginScreen import Ui_MainWindow
 from windows.openWindow import openWindow
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
 
 openWindow(Ui_MainWindow)
